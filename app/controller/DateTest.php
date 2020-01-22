0b3c9bf0a81e47d9287d16c26c590d45a12f81c4
小人物 <491376424@qq.com>
2020-01-22 23:59:14 +0800

    P1111.数据库的修改删除

--- app/controller/DateTest.php
+++ app/controller/DateTest.php
@@ -17,4 +17,29 @@ class DateTest{
 		//\dump($thh);
 		return Db::getLastSql();
 	}
+	public function insert(){
+		$date['thh_name'] = 1;
+		$date['thh_phone'] = 1;
+		$date['thh_address'] = 1;
+		$date['thh_yw'] = 1;
+		$date['thh_ks'] = 1;
+		$date['thh_cm'] = 1;
+		$date['thh_is_cj'] = 1;
+		$date['thh_is_jh'] = 1;
+		$date['thh_is_dh'] = 1;
+		return Db::name("thh")->insert($date);
+	}
+	public function update(){
+		$date['thh_id']=195;
+		$date['thh_name'] = 111;
+		$date['thh_phone'] = 2;
+		$date['thh_address'] = 2;
+		$date['thh_yw'] = 2;
+		$date['thh_ks'] = 2;
+		$date['thh_cm'] = 2;
+		$date['thh_is_cj'] = 2;
+		$date['thh_is_jh'] = 2;
+		$date['thh_is_dh'] = 2;
+		return Db::name('thh')->update($date);
+	}
 }
